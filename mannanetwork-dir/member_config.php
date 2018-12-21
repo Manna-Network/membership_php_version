d8f70b438bf91f83c0797a26b85ad46ecc2b036a
Robert Lefebvre <robert@bungeebones.com>
2018-12-20 21:32:09 -0500

    Update member_config.php

--- mannanetwork-dir/member_config.php
+++ mannanetwork-dir/member_config.php
@@ -1,5 +1,5 @@
 <?php
-//Both these settings can be gotten from your agent upline. 
+//All of these settings can be gotten from your agent upline. 
 $agent_url = "1stwebtrafficbank.com";
 $agent_folder = "bitcoin_ad_agency/mannanetwork-dir";
 $registration_folder = "bitcoin_ad_agency/manna-network";//used by the endorsements/add-url.php in the iframe to load agent registration page
