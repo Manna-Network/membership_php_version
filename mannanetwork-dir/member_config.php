edc7c9e8d1ffe06f23968c4c983dcbdd6ed056ce
Robert Lefebvre <robert@bungeebones.com>
2018-12-22 18:47:39 -0500

    Update member_config.php

--- mannanetwork-dir/member_config.php
+++ mannanetwork-dir/member_config.php
@@ -3,4 +3,5 @@
 $agent_url = "1stwebtrafficbank.com";
 $agent_folder = "bitcoin_ad_agency/mannanetwork-dir";
 $registration_folder = "bitcoin_ad_agency/manna-network";//used by the endorsements/add-url.php in the iframe to load agent registration page
+$lnk_num = 1; //this would be your link numbergiven by your agent
 ?>
